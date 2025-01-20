a404656ad7fd52082159992fda5cc21d49028297
bartekstrojek <bartlomiejstrojek69@gmail.com>
2025-01-20 13:09:31 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [user, setUser] = useState("null");
+
+  const getData = () => {
+    fetch("https://randomuser.me/api/")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Name</h1>
+      <img src="null" alt="" />
+      <p>Mail</p>
+      <button onClick={getData}>Click</button>
     </div>
   );
 }
